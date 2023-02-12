133aeb5a19cd96998cdd25e3f03720b2ba8e7f98
Nabetani <takenori@nabetani.sakura.ne.jp>
2023-02-12 16:45:33 +0900

    タイマー有効タブのタイトルリストを得る

--- package/impl/background.js
+++ package/impl/background.js
@@ -3,43 +3,56 @@
 console.log("background task started.");
 let timerIds = {};
 
+async function prepareRes() {
+    console.log(prepareRes);
+    let tabs = [];
+    for (const [tabId, timerId] of Object.entries(timerIds)) {
+        if (null != timerId) {
+            let tab = await chrome.tabs.get(parseInt(tabId, 10));
+            tabs.push(tab.title);
+        }
+    }
+    console.log("tabs is", tabs);
+    return { tabs: tabs };
+}
+
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        if ("enable" != request.cmd && "disable" != request.cmd) {
-            return;
-        }
-        ((tabId, cmd, intervalMS) => {
-            const timerId = () => { return timerIds[tabId] };
-            const disable = () => {
-                if (timerId()) {
-                    console.log("disable interval timer for ", tabId,);
-                    clearInterval(timerId());
-                    timerIds[tabId] = null;
-                }
-            };
-            const reload = () => {
-                chrome.tabs.reload(tabId, { bypassCache: true }, () => {
-                    const e = chrome.runtime.lastError;
-                    if (e) {
-                        console.log("failed to reload tab ", tabId, ": ", e, " @ ", (new Date()).toJSON());
-                        disable();
-                    } else {
-                        console.log("reloaded tab ", tabId, ": ", e, " @ ", (new Date()).toJSON());
+        if ("enable" == request.cmd || "disable" == request.cmd) {
+            ((tabId, cmd, intervalMS) => {
+                const timerId = () => { return timerIds[tabId] };
+                const disable = () => {
+                    if (timerId()) {
+                        console.log("disable interval timer for ", tabId,);
+                        clearInterval(timerId());
+                        timerIds[tabId] = null;
                     }
-                });
-            };
-            const enable = () => {
-                disable();
-                timerIds[tabId] = setInterval(reload, intervalMS);
-            };
-            switch (cmd) {
-                case "enable":
-                    enable();
-                    break;
-                case "disable":
+                };
+                const reload = () => {
+                    chrome.tabs.reload(tabId, { bypassCache: true }, () => {
+                        const e = chrome.runtime.lastError;
+                        if (e) {
+                            console.log("failed to reload tab ", tabId, ": ", e, " @ ", (new Date()).toJSON());
+                            disable();
+                        } else {
+                            console.log("reloaded tab ", tabId, ": ", e, " @ ", (new Date()).toJSON());
+                        }
+                    });
+                };
+                const enable = () => {
                     disable();
-                    break;
-            }
-        })(request.tab.id, request.cmd, request.intervalMS);
+                    timerIds[tabId] = setInterval(reload, intervalMS);
+                };
+                switch (cmd) {
+                    case "enable":
+                        enable();
+                        break;
+                    case "disable":
+                        disable();
+                        break;
+                }
+            })(request.tab.id, request.cmd, request.intervalMS);
+        }
+        prepareRes().then((o) => { sendResponse(o); });
     }
 );
--- package/impl/popup.js
+++ package/impl/popup.js
@@ -1,18 +1,28 @@
 
 "use strict";
 
-const regEvent = async (cmd, f) => {
+const send = async (obj) => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  return chrome.runtime.sendMessage(Object.assign({ tab: tab }, obj));
+};
+
+let regres = null;
+
+const regEvent = async (cmd) => {
   const val = (id) => {
     return parseFloat(document.getElementById(id).value);
   };
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  await chrome.runtime.sendMessage({ cmd: cmd, tab: tab, intervalMS: val("num") * val("unit") });
+  return send({ cmd: cmd, intervalMS: val("num") * val("unit") });
+};
+
+const updateTimerList = (o) => {
+  console.log(o);
 };
 
 window.onload = () => {
   console.log("onload");
+  send({ cmd: "none" }).then(updateTimerList);
   chrome.storage.sync.get(null, (val) => {
-    console.log("chrome.storage.sync.get");
     if (val && val.num && val.unit) {
       document.getElementById("num").value = val.num;
       document.getElementById("unit").value = val.unit;
@@ -21,11 +31,13 @@ window.onload = () => {
 };
 
 document.getElementById("enable").addEventListener("click", async () => {
-  await regEvent("enable", () => { });
+  let o = await regEvent("enable");
+  updateTimerList(o);
   const val = (id) => { return document.getElementById(id).value; };
   chrome.storage.sync.set({ "num": val("num"), "unit": val("unit") });
 });
 
 document.getElementById("disable").addEventListener("click", async () => {
-  await regEvent("disable", () => { });
+  let o = await regEvent("disable");
+  updateTimerList(o);
 });
